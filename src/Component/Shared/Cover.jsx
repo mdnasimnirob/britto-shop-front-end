d34769825b2f1971f82ce3408985664e959b1cda
nasimnirob <nasimnirob509668@gmail.com>
2025-05-05 07:26:52 +0600

    working

--- src/Component/Shared/Cover.jsx
+++ src/Component/Shared/Cover.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Parallax } from 'react-parallax';
 import Banner from '../../assets/cover/banner.jpg'
-import img2 from'../../assets/cover/istockphoto-1148925683-612x612.jpg'
+import img2 from '../../assets/cover/istockphoto-1148925683-612x612.jpg'
 import SectionTittle from '../SectionTittle/SectionTittle';
 
 
@@ -13,25 +13,30 @@ const Cover = () => {
                 blur={{ min: -30, max: 30 }}
                 bgImage={Banner}
                 bgImageAlt="A scenic background"
-               
+
             >
-                
-                <div className='hero' style={{ height: '600px' }}>
-                <div className='border'>
-                <SectionTittle></SectionTittle>
-                </div>
-                <div className="hero-overlay bg-opacity-60 bg-black"></div>
-                <div  className="hero-content text-neutral-content text-center flex-col lg:flex-row">
-                    <img className='w-1/2' src={img2} alt="" />
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold ">Britto Shop</h1>
-                        <p className="mb-5">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                        <button className="btn btn-outline border-b-4 text-orange-500 border-0">Read More</button>
+
+                    <div className='hero-overlay bg-opacity-60 bg-black py-0.5'>
+                        <SectionTittle 
+                        subHeading={'Check it out'} 
+                        heading={'From Our Menu'}
+                        ></SectionTittle>
                     </div>
-                </div>
+                <div className='hero' style={{ height: '600px' }}>
+                    <div className="hero-overlay bg-opacity-60 bg-black"></div>
+                    <div className="hero-content text-neutral-content text-center flex-col lg:flex-row">
+                        <img className='w-1/2 rounded transition-transform duration-300 transform hover:scale-105' src={img2} alt="" />
+                        <div className="max-w-md">
+                            <h1 className="mb-7 text-5xl font-bold text-orange-500">ব্রিট্টো শপ</h1>
+                            <p className="mb-7">
+                                আমরা মানসম্মত পণ্য ও সেবা দিয়ে থাকি গ্রাহকের সন্তুষ্টিকে অগ্রাধিকার দিয়ে। প্রতিদিনের প্রয়োজনীয় সবকিছু এক ছাদের নিচে।
+                                <span>
+                                    আমাদের এখানে আপনি পাবেন নিত্যপ্রয়োজনীয় পণ্য, গৃহস্থালি সামগ্রী, ফ্যাশন আইটেমসহ আরও অনেক কিছু। সাশ্রয়ী দামে সেরা পণ্য নিয়ে আমরা সবসময় প্রস্তুত।
+                                </span>
+                            </p>
+                            <button className="btn btn-outline border-b-4 text-orange-500 border-0">Read More</button>
+                        </div>
+                    </div>
                 </div>
             </Parallax>
         </>
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Error = () => {
     return (
         <div className='flex h-screen justify-center items-center flex-col gap-4'>
-            <h1 className='text-6xl text-red-500 '>Page Not Found | 502</h1>
-            <NavLink to='/'><p className='btn btn-info text-lg'>go to home</p></NavLink>
+            <h1 className='text-6xl text-red-500 '>Page Not Found | 404</h1>
+            <NavLink to='/'><p className='btn btn-error text-lg'>go to home</p></NavLink>
         </div>
     );
 };
